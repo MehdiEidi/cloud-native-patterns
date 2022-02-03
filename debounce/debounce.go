58a0546e355ac4ca3acebdb7075b395f7e37fb08
MehdiEidi <mehdiadq@gmail.com>
2022-02-04 00:44:17 +0330

    debounceLast pattern added

--- debounce/debounce.go
+++ debounce/debounce.go
@@ -31,3 +31,53 @@ func DebounceFirst(circuit Circuit, d time.Duration) Circuit {
 		return result, err
 	}
 }
+
+func DebounceLast(circuit Circuit, d time.Duration) Circuit {
+	threshold := time.Now()
+	var ticker *time.Ticker
+	var result string
+	var err error
+	var once sync.Once
+	var lock sync.Mutex
+
+	return func(ctx context.Context) (string, error) {
+		lock.Lock()
+		defer lock.Unlock()
+
+		threshold = time.Now().Add(d)
+
+		once.Do(func() {
+			ticker = time.NewTicker(time.Millisecond * 100)
+
+			go func() {
+				defer func() {
+					lock.Lock()
+					ticker.Stop()
+					once = sync.Once{}
+					lock.Unlock()
+				}()
+
+				for {
+					select {
+					case <-ticker.C:
+						lock.Lock()
+						if time.Now().After(threshold) {
+							result, err = circuit(ctx)
+							lock.Unlock()
+							return
+						}
+						lock.Unlock()
+
+					case <-ctx.Done():
+						lock.Lock()
+						result, err = "", ctx.Err()
+						lock.Unlock()
+						return
+					}
+				}
+			}()
+		})
+
+		return result, err
+	}
+}

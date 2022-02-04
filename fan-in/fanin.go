aa86b35cbc59e820b94b986643ccdf388889f960
MehdiEidi <mehdiadq@gmail.com>
2022-02-04 14:32:53 +0330

    fan in pattern added

---
+++ fan-in/fanin.go
@@ -0,0 +1,28 @@
+package fanin
+
+import "sync"
+
+func Funnel(sources ...<-chan int) <-chan int {
+	dest := make(chan int)
+
+	var wg sync.WaitGroup
+
+	wg.Add(len(sources))
+
+	for _, ch := range sources {
+		go func(ch <-chan int) {
+			defer wg.Done()
+
+			for n := range ch {
+				dest <- n
+			}
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(dest)
+	}()
+
+	return dest
+}
